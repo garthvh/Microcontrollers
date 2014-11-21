e95d83dcb93d9b2b4926e5e3d1cb971c6c400fd6
Garth Vander Houwen <garthvh@yahoo.com>
2014-11-20 23:09:01 -0800

    Update header

--- 8x8Examples.ino
+++ 8x8Examples.ino
@@ -1,22 +1,6 @@
 /*************************************************** 
-  This is a library for our I2C LED Backpacks
-
-  Designed specifically to work with the Adafruit LED Matrix backpacks 
-  ----> http://www.adafruit.com/products/872
-  ----> http://www.adafruit.com/products/871
-  ----> http://www.adafruit.com/products/870
-
-  These displays use I2C to communicate, 2 pins are required to 
-  interface. There are multiple selectable I2C addresses. For backpacks
-  with 2 Address Select pins: 0x70, 0x71, 0x72 or 0x73. For backpacks
-  with 3 Address Select pins: 0x70 thru 0x77
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
+  Assorted Examples for use with the Adafruit HT16K33 
+  Breakout board connected to an 8x8 LED matrix
  ****************************************************/
 
 #include <Wire.h>
